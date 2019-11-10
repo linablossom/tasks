c1805570ea34b1f4e36c59894d0e89471a015e89
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-11 00:04:35 +0200

    Update hw-08-11.js

--- hw-08-11.js
+++ hw-08-11.js
@@ -1,1 +1,35 @@
+// https://www.codewars.com/kata/opposite-number/train/javascript
 
+function opposite(number) {
+  return -number;
+}
+
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet (name, owner) {
+  return name === owner
+    ? 'Hello boss'
+    : 'Hello guest';
+}
+
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move (position, roll) {
+  return position + roll * 2;
+  // return the new position
+}
